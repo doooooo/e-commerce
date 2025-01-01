0a0eb658bd37596de94d594d5fd1bf0cafea98af
doaa <doaa.farouk07@gmail.com>
2025-01-01 11:56:34 +0200

    complete DB schema - adding admin portal

--- init.sql
+++ init.sql
@@ -4,17 +4,50 @@ CREATE DATABASE testdb;
 
 USE testdb;
 CREATE TABLE PRODUCT (
-  `ID` int NOT NULL,
+  `ID` int NOT NULL AUTO_INCREMENT primary key,
   `Category` varchar(45) DEFAULT NULL,
   `NAME` varchar(150) DEFAULT NULL,
-  `Price` DECIMAL(10,2) NOT NULL
+  `Price` DECIMAL(10,2) NOT NULL,
+  `Photo` varchar(250) DEFAULT NULL
 );
 
-INSERT INTO PRODUCT
-VALUES (1, 'Sweats', 'Choco 1', 12.4);
+CREATE TABLE USER (
+  `ID` int NOT NULL AUTO_INCREMENT primary key,
+  `NAME` varchar(200) NOT NULL,
+  `USERID` varchar(100) NOT NULL,
+  `PASSWORD` varchar(100) NOT NULL
+);
 
-INSERT INTO PRODUCT
-VALUES (2, 'Sweats', 'Chips', 15.0);
+CREATE TABLE PURCHASES (
+  `USERID` int NOT NULL,
+  `PRODUCTID` int NOT NULL,
+  `Date` DATETIME DEFAULT NULL,
+  `Qty` int NOT NULL,
+  `Price` DECIMAL(10,2) NOT NULL,
+  `IS_CART` BOOLEAN DEFAULT FALSE
+);
 
-INSERT INTO PRODUCT
-VALUES (3, 'Meat', 'Steak', 200.50);
+CREATE TABLE ADMIN (
+  `ID` int NOT NULL AUTO_INCREMENT primary key,
+  `NAME` varchar(200) NOT NULL,
+  `USERID` varchar(100) NOT NULL,
+  `PASSWORD` varchar(100) NOT NULL
+);
+
+INSERT INTO PRODUCT (`Category`, `NAME`, `Price`)
+VALUES ('Candy', 'Choco 1', 12.4);
+
+INSERT INTO PRODUCT (`Category`, `NAME`, `Price`)
+VALUES ('Candy', 'Choco 2', 20.5);
+
+INSERT INTO PRODUCT (`Category`, `NAME`, `Price`)
+VALUES ('Snacks', 'Chips', 15.0);
+
+INSERT INTO PRODUCT (`Category`, `NAME`, `Price`)
+VALUES ('Meat', 'Steak', 200.50);
+
+INSERT INTO PRODUCT (`Category`, `NAME`, `Price`)
+VALUES ('Poultry', 'Chicken Fajita', 175.50);
+
+INSERT INTO ADMIN (`NAME`,`USERID`,`PASSWORD`)
+VALUES ('admin user','admin','admin@123')
