21c181e478a439e78389496c79ee20e3a8dea7f6
doaa <doaa.farouk07@gmail.com>
2025-01-06 15:36:21 +0200

    admin products page

--- app/admin/index.php
+++ app/admin/index.php
@@ -2,10 +2,30 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<?php include '../connect.php';
+echo "<h1>My first PHP page</h1>";
+$sql="SELECT * FROM PRODUCT";
+$ret = mysqli_query ($conn, $sql);
+if(mysqli_num_rows($ret) > 0)
+{
+    echo "<div>";
+	echo "<ul>";
+	while($row=mysqli_fetch_assoc($ret))
+	{
+		#echo "EMP ID: {$row['id']}<br/> ". "EMP NAME: {$row['id']}<br/>". "EMP salary: {$row['salary']}<br/>";
+		echo "<li> <ul>";
+		echo "<li>". $row['Category']."</li>";
+		echo "<li>". $row['NAME']."</li>";
+		echo "<li>". $row['Price']."</li>";
+		echo "</ul>";
+	}
+	echo "</ul>";
+    echo "</div>";
+}
+else echo "0 results found";
 
-<?php
-echo "Hello World!";
+mysqli_free_result($ret);
+mysqli_close($conn);
 ?>
 
 </body>
---
+++ app/connect.php
@@ -0,0 +1,47 @@
+<?php  
+$host = 'localhost';   $user = 'springstudent';  $pass = 'springstudent';    
+$dbname = 'testdb';  $port=3308;
+  
+$conn = mysqli_connect ($host, $user, $pass, $dbname, $port);  
+if(!$conn){  
+      die('Could not connect: '.mysqli_connect_error());  
+}  
+/*echo 'Connected successfully <br/>'; 
+
+#$sql="SELECT * FROM emp WHERE salary >2100";
+$n = $_GET['nameee'];
+$s = $_GET['sal'];
+$add = $_GET['add'];
+
+$sql="INSERT INTO emp (namee, salary, address) VALUES ('$n', '$s', '$add')";
+$ret = mysqli_query ($conn, $sql);
+
+if (!$ret)
+	echo "Could not insert";
+else
+	echo "Data inserted successfully";
+
+
+$sql="SELECT * FROM emp";
+$ret = mysqli_query ($conn, $sql);
+if(mysqli_num_rows($ret) > 0)
+{
+	echo "<ol>";
+	while($row=mysqli_fetch_assoc($ret))
+	{
+		#echo "EMP ID: {$row['id']}<br/> ". "EMP NAME: {$row['id']}<br/>". "EMP salary: {$row['salary']}<br/>";
+		echo "<li> <ul>";
+		echo "<li>". $row['id']."</li>";
+		echo "<li>". $row['namee']."</li>";
+		echo "<li>". $row['salary']."</li>";
+		echo "<li> {$row['address']} </li>";
+		echo "</ul>";
+	}
+	echo "</ol>";
+}
+else echo "0 results found";
+
+mysqli_free_result($ret);
+mysqli_close($conn);*/
+
+?>
